22d8185e544eb3af23afce759a5b56a3c3907c42
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-09-17 11:11:14 -0400

    bla

---
+++ sandwich.py
@@ -0,0 +1,31 @@
+# Steps of making a sandwiiiiiich
+print("we r making sandwich.")
+print("grab slice of bread")
+print("butter")
+print("spread peanut butter on bread")
+print("spread jam on bread")
+print("enjoy")
+
+# voting algorithm
+# age = int(input('whats ur age'))
+# citizen_status = input('r u us citizen Y/n')
+# if citizen_status == 'Y':
+#     if age >= 18:
+#         print('u can vote!')
+#     elif age<18:
+#         print("can't vote")
+#     else:
+#         print('enter valid input')
+# elif citizen_status == 'n':
+#     print("u can't vote s")
+# else:
+#     print('input not valid')
+
+# fav #
+fav_num = int(input("enter ur fav #"))
+if fav_num % 2 == 0:
+    print("ur num is even")
+elif fav_num % 2 == 1:
+    print("ur fav num is odd")
+else:
+    print("input not valid")
